f3fe666af003cf9284e623c52b6d12414a39ffd3
omar alhaj ali <omar18yyyy@gmail.com>
2025-05-30 23:17:10 +0300

    bugs and some edit

--- examples/usage.ts
+++ examples/usage.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {BtuidGenerator,} from '../src/btuid'
+import {BtuidGenerator,} from '../src/btuid'//'btuid' if install it from npm
 import path from 'path';
 
 
